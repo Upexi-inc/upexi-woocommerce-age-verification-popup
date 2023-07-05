f3da68becafee8fe5688083abe562ca1ca9b8afe
tylly <jamestylladams@gmail.com>
2023-07-05 12:35:07 -0400

    config file comment block

---
+++ upexi-age-verification-popup.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Upexi Age Verification Popup for Woocommerce
+ * Plugin URI: https://github.com/tylly
+ * Description: Implement custom age verification popup for Woocommerce.
+ * Author: Upexi Inc.
+ * Version: 1.0
+ * Author URI: https://github.com/tylly
+ */
+
+ 
