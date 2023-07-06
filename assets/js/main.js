2d7319d034a31e8ed834534b5b063462aad76c72
upexiAdmin <130496281+upexiAdmin@users.noreply.github.com>
2023-07-06 23:13:25 +0100

    Small refactor

---
+++ assets/js/main.js
@@ -0,0 +1,58 @@
+// Helper functions to deal with localStorage
+// Function to set a value in localStorage
+function setItem(key, value) {
+    try {
+        if (typeof value === 'object') {
+            value = JSON.stringify(value);
+        }
+        localStorage.setItem(key, value);
+    } catch (error) {
+        console.error('Local Storage Set Error: ', error);
+    }
+}
+
+// Function to get a value from localStorage
+function getItem(key) {
+    try {
+        let value = localStorage.getItem(key);
+        try {
+            // Attempt to parse the value as JSON. If it fails, it's a primitive.
+            value = JSON.parse(value);
+        } catch (error) {
+            // Value is not JSON, do nothing and return it as is
+        }
+        return value;
+    } catch (error) {
+        console.error('Local Storage Get Error: ', error);
+        return null;
+    }
+}
+
+// JavaScript code to handle the age verification logic
+(function() {
+    // Function to handle age confirmation
+    function handleAgeConfirmation(popup, btn) {
+        btn.addEventListener('click', function() {
+            // Store verification status in local storage
+            setItem('age_verified', 'true');
+            
+            // Remove the popup from the DOM if it exists and it has 'closed' class
+            if (popup && popup.classList.contains('closed')) {
+                popup.classList.remove('closed');
+                popup.remove();
+            }
+        });
+    }
+
+    // Check if age verification is required
+    const popup = document.getElementById('age-verification-popup');
+    const btn = document.getElementById('confirm-age');
+    
+    if (!getItem('age_verified') && popup && btn) {
+        handleAgeConfirmation(popup, btn);
+    } else {
+        console.warn('Age verification elements not found in the DOM.');
+    }
+})();
+
+
--- upexi-age-verification-popup.php
+++ upexi-age-verification-popup.php
@@ -8,15 +8,20 @@
  * Author URI: https://github.com/tylly
  */
 
-register_activation_hook(__FILE__, 'uap_install');
 
-function uap_install() {
-    // Activation logic goes here
-}
 
-add_action('wp_footer', 'homepage_popup_render');
+add_action('wp_footer', 'popup_render');
 
-function homepage_popup_render() {
+function upexi_age_verification_scripts() {
     // Code to render the popup goes here
-    include 'views/public/popup.php';
+    include(plugin_dir_path(__FILE__) . 'views/public/popup.php');
 }
+
+
+add_action( 'wp_enqueue_scripts', 'upexi_age_verification_scripts' );
+
+function upexi_age_verification_scripts() {
+    wp_enqueue_style( 'upexi_age_verification_style', plugins_url( '/assets/styles/popup.css', __FILE__ ), array(), '1.0' );
+    wp_enqueue_script( 'upexi_age_verification_script', plugins_url( '/assets/js/main.css', __FILE__ ), array(), '1.0', true );
+}
+
